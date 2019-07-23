6a39e1a1647cb4b1b3cf9a9f61ac9e19ec8f1cb6
lianhj <lianhj18@qq.com>
2019-07-23 11:00:28 +0800

    lidar grid & single obstacle

--- src/MPC_data.cpp
+++ src/MPC_data.cpp
@@ -18,9 +18,10 @@ protected:
     ros::Subscriber can_sub;
     ros::Publisher radar_pub;
     ros::Publisher camera_pub;
-    ros::Publisher rawobj_pub;
+    ros::Publisher radar_obj_pub;
+    ros::Publisher camera_obj_pub;
     ros::Publisher fusion_pub;
-    ros::Publisher obj_pub;
+    ros::Publisher nearest_obj_pub;
     std::string fixed_frame;
     float x_offset;
 
@@ -30,9 +31,10 @@ public:
         can_sub = nh.subscribe("received_messages", 1, &MPCDataHandler::canHandler, this);    //接收话题：received_messages
         radar_pub = nh.advertise<geometry_msgs::Point>("radar_pos", 1);                       //发布话题：radar_pos
         camera_pub = nh.advertise<geometry_msgs::Point>("camera_pos", 1);                     //发布话题：camera_pos
-        rawobj_pub = nh.advertise<visualization_msgs::MarkerArray>("raw_objects", 1);         //发布话题：raw_objects
+        radar_obj_pub = nh.advertise<visualization_msgs::Marker>("radar_obj", 1);             //发布话题：radar_obj
+        camera_obj_pub = nh.advertise<visualization_msgs::Marker>("camera_obj", 1);           //发布话题：camera_obj
         fusion_pub = nh.advertise<geometry_msgs::Point>("fusion_pos", 1);                     //发布话题：fusion_pos
-        obj_pub = nh.advertise<visualization_msgs::Marker>("nearest_obj", 1);                 //发布话题：nearest_obj
+        nearest_obj_pub = nh.advertise<visualization_msgs::Marker>("nearest_obj", 1);         //发布话题：nearest_obj
 
         nh.getParam("/MPC_data/fixed_frame", fixed_frame);
         nh.getParam("/MPC_data/x_offset", x_offset);
@@ -50,7 +52,6 @@ void MPCDataHandler::canHandler(const can_msgs::Frame& input)
     if(input.id == 0x563)
     {
         geometry_msgs::Point radar_pos;
-        geometry_msgs::Point camera_pos;
 
         radar_pos.x = (float)(input.data[0]*256 + input.data[1])/100;
 
@@ -68,15 +69,55 @@ void MPCDataHandler::canHandler(const can_msgs::Frame& input)
         {
             radar_pos.y = (radar_pos.y - 0x10000)/10;
         }
+
+        radar_pub.publish(radar_pos);     //发布毫米波雷达目标位置
+
+        visualization_msgs::Marker radar_obj;
+
+        radar_obj.header.frame_id = fixed_frame;
+        radar_obj.header.stamp = ros::Time::now();
+        radar_obj.id = 0;
+        radar_obj.type = visualization_msgs::Marker::CUBE;
+        radar_obj.action = visualization_msgs::Marker::ADD;
+        radar_obj.pose.position.x = radar_pos.x;
+        radar_obj.pose.position.y = radar_pos.y;
+        radar_obj.pose.position.z = -0.9;
+        radar_obj.pose.orientation.x = 0.0;
+        radar_obj.pose.orientation.y = 0.0;
+        radar_obj.pose.orientation.z = 0.0;
+        radar_obj.pose.orientation.w = 1.0;
+        radar_obj.scale.x = 0.6;
+        radar_obj.scale.y = 0.6;
+        radar_obj.scale.z = 1.7;
+        radar_obj.color.r = 0;
+        radar_obj.color.g = 0;
+        radar_obj.color.b = 1;
+
+        if(radar_pos.x == 0)
+        {
+            radar_obj.color.a = 0;
+        }
+        else
+        {
+            radar_obj.color.a = 0.8;
+        }
         
-        camera_pos.x = (float)(input.data[4]*256 + input.data[5])/100;
+        radar_obj.lifetime = ros::Duration();
+
+        radar_obj_pub.publish(radar_obj);    //发布毫米波目标几何形状
+    }
+    else if(input.id == 0x564)
+    {
+        geometry_msgs::Point camera_pos;
+
+        camera_pos.x = (float)(input.data[0]*256 + input.data[1])/100;
 
         if(camera_pos.x > 0)
         {
             camera_pos.x = camera_pos.x + x_offset;
         }
 
-        camera_pos.y = (float)(input.data[6]*256 + input.data[7]);
+        camera_pos.y = (float)(input.data[2]*256 + input.data[3]);
         if(camera_pos.y < 0x8000)
         {
             camera_pos.y = camera_pos.y / 16;
@@ -86,82 +127,61 @@ void MPCDataHandler::canHandler(const can_msgs::Frame& input)
             camera_pos.y = (camera_pos.y - 0x10000)/16;
         }
 
-        radar_pub.publish(radar_pos);     //发布毫米波雷达目标位置
         camera_pub.publish(camera_pos);   //发布摄像头目标位置
 
-        visualization_msgs::MarkerArray objects;
-        objects.markers.resize(2);
-        std::vector<Objects> ped;
-        ped.resize(2);
-        ped[0].x = radar_pos.x;
-        ped[0].y = radar_pos.y;
-        ped[1].x = camera_pos.x;
-        ped[1].y = camera_pos.y;
+        visualization_msgs::Marker camera_obj;
 
-        for(i=0; i<2; ++i)
+        camera_obj.header.frame_id = fixed_frame;
+        camera_obj.header.stamp = ros::Time::now();
+        camera_obj.id = 0;
+        camera_obj.type = visualization_msgs::Marker::CUBE;
+        camera_obj.action = visualization_msgs::Marker::ADD;
+        camera_obj.pose.position.x = camera_pos.x;
+        camera_obj.pose.position.y = camera_pos.y;
+        camera_obj.pose.position.z = -0.9;
+        camera_obj.pose.orientation.x = 0.0;
+        camera_obj.pose.orientation.y = 0.0;
+        camera_obj.pose.orientation.z = 0.0;
+        camera_obj.pose.orientation.w = 1.0;
+        camera_obj.scale.x = 0.6;
+        camera_obj.scale.y = 0.6;
+        camera_obj.scale.z = 1.7;
+        camera_obj.color.r = 0;
+        camera_obj.color.g = 1;
+        camera_obj.color.b = 0;
+
+        if(camera_pos.x == 0)
         {
-            objects.markers[i].header.frame_id = fixed_frame;
-            objects.markers[i].header.stamp = ros::Time::now();
-            objects.markers[i].type = visualization_msgs::Marker::CYLINDER;
-            objects.markers[i].action = visualization_msgs::Marker::ADD;
-            objects.markers[i].id = i+1;
-            objects.markers[i].pose.position.x = ped[i].x;
-            objects.markers[i].pose.position.y = ped[i].y;
-            objects.markers[i].pose.position.z = -0.9;
-            objects.markers[i].pose.orientation.x = 0.0;
-            objects.markers[i].pose.orientation.y = 0.0;
-            objects.markers[i].pose.orientation.z = 0.0;
-            objects.markers[i].pose.orientation.w = 1.0;
-            objects.markers[i].scale.x = 0.6;
-            objects.markers[i].scale.y = 0.6;
-            objects.markers[i].scale.z = 1.7;
-
-            if(i == 0)
-            {
-                objects.markers[i].color.r = 0;
-                objects.markers[i].color.g = 0;
-                objects.markers[i].color.b = 1;
-            }
-            else
-            {
-                objects.markers[i].color.r = 1;
-                objects.markers[i].color.g = 0;
-                objects.markers[i].color.b = 0;
-            }
-            
-            if(ped[i].x == 0)
-            {
-                objects.markers[i].color.a = 0;
-            }
-            else
-            {
-                objects.markers[i].color.a = 0.7;
-            }
-            
-            objects.markers[i].lifetime = ros::Duration();
+            camera_obj.color.a = 0;
         }
+        else
+        {
+            camera_obj.color.a = 0.8;
+        }
+        
+        camera_obj.lifetime = ros::Duration();
 
-        rawobj_pub.publish(objects);
+        camera_obj_pub.publish(camera_obj);    //发布摄像头目标几何形状
     }
-    else if(input.id == 0x564)
+    else if(input.id == 0x565)
     {
         geometry_msgs::Point fusion_pos;
 
-        fusion_pos.x = (float)(input.data[0]*256 + input.data[1])/100;
+        fusion_pos.x = (input.data[0]*256 + input.data[1])/100;
 
         if(fusion_pos.x > 0)
         {
             fusion_pos.x = fusion_pos.x + x_offset;
         }
 
-        fusion_pos.y = (float)(input.data[4]*256 + input.data[5]);
+        fusion_pos.y = input.data[2]*256 + input.data[3];
         if(fusion_pos.y < 0x8000)
         {
-            fusion_pos.y = fusion_pos.y / 16;
+            fusion_pos.y = fusion_pos.y / 80;
         }
         else
         {
-            fusion_pos.y = (fusion_pos.y - 0x10000)/16;
+            fusion_pos.y = (fusion_pos.y - 0x10000) / 80;
         }
 
         fusion_pub.publish(fusion_pos);   //发布毫米波雷达和摄像头的融合位置
@@ -198,7 +218,7 @@ void MPCDataHandler::canHandler(const can_msgs::Frame& input)
         
         nearest.lifetime = ros::Duration();
 
-        obj_pub.publish(nearest);    //发布最近目标几何形状
+        nearest_obj_pub.publish(nearest);    //发布最近目标几何形状
     }
 }
 
